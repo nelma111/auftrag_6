2275188c07c9a2a7df32de45086b1d930e4d5633
nelma111 <max.nell@usz.ch>
2020-06-03 16:21:10 +0200

    try to add moblie menu
    fundamentals for moble menu
    menu desktop improvements

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function hideMenu() {
         x.classList.add("hidden");
     }
 }
+
+function showMenu_moblie() {
+    document.getElementById("body").style.height = "140vh";
+    document.getElementById("news").style.display = "none";
+    document.getElementById("trending").style.display = "none"
+    document.getElementById("happening_now").style.display = "none"
+    var x = document.getElementById("header");
+    x.classList.add("header_open");
+    var z = document.getElementById("footer");
+    z.classList.remove("footer_mobile");
+    z.classList.add("footer_open");
+}
