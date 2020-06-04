5b4bf3ff0b04d229a5a73adaf0aaf4b0a21badd6
nelma111 <max.nell@usz.ch>
2020-06-04 12:10:18 +0200

    added mobile_menu
    hat noch kein links aber gui fertig

--- script.js
+++ script.js
@@ -12,13 +12,24 @@ function hideMenu() {
 }
 
 function showMenu_moblie() {
-    document.getElementById("body").style.height = "140vh";
+    document.getElementById("body").style.height = "100vh";
     document.getElementById("news").style.display = "none";
-    document.getElementById("trending").style.display = "none"
-    document.getElementById("happening_now").style.display = "none"
+    document.getElementById("trending").style.display = "none";
+    document.getElementById("happening_now").style.display = "none";
     var x = document.getElementById("header");
-    x.classList.add("header_open");
+    x.classList.remove("header");
+    x.classList.add("header_mobile")
     var z = document.getElementById("footer");
     z.classList.remove("footer_mobile");
     z.classList.add("footer_open");
+    document.getElementById("openmenu_btn").style.display = "none";
+
+    document.getElementById("nav_mobile").style.display = "unset";
+
+    document.getElementById("icon1").style.display = "unset";
+    document.getElementById("icon2").style.display = "unset";
+    document.getElementById("icon3").style.display = "unset";
+    document.getElementById("icon4").style.display = "unset";
+    document.getElementById("icon5").style.display = "unset";
+    document.getElementById("nav_with_pp").style.flexdirection = "column";
 }
